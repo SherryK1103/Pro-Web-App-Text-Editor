1cc1c14d4bd12c982ed81e05445cea8704544b9e
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-29 19:54:06 -0500

    added get and put functions to database js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  try {
+    const result = await store.add({ content });
+    console.log('Data stored with ID' + result);
+  } catch (error) {
+    console.error('putDb not implemented' + error);
+  }
+  await tx.done;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  try {
+    const content = await store.getAll();
+    console.log('Retrieved data: ', content);
+    return content;
+  } catch (error) {
+    console.error('getDb not implemented' + error);
+    return [];
+  }
+};
+
 
 initdb();
